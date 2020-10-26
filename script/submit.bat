25a5141e72ec9d5a90e350b5964ceaa834675c54
Zora <1005674489@qq.com>
2020-10-26 22:52:55 +0800

    add new article

--- script/submit.bat
+++ script/submit.bat
@@ -1,6 +1,7 @@
+@echo off
 call git add .
 call git commit -m "add new article"
-call git rebase origin master
+call git rebase origin/master
 call git push origin master
 call git push gitee master
 call ./script/deploy.bat
